4254cc496c3cae498e87ea294b10ddda270c5b36
James Lungu <james.lungu@stonybrook.edu>
2020-06-06 19:38:54 -0400

    SO. Added search bar and finished routes. Just poking around for a module implementing autocomplete for search bar...

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 
 export class HomeComponent extends Component {
-    render(){
-        return(
-            <div>
-                Home!
+  render() {
+    return (
+      <div class="home-div">
+        <div class="input-group input-group-lg main-search">
+          <input
+            type="text"
+            class="form-control"
+            aria-label="Large"
+            aria-describedby="inputGroup-sizing-sm"
+          />
+            <div class="input-group-append">
+                <button class="btn btn-outline-secondary" type="button">Submit</button>
             </div>
-        );
-    }
+        </div>
+      </div>
+    );
+  }
 }
 export default HomeComponent;
