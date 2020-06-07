01c55dc3b78beb9dad09cc71d3cf08b4bda67905
James Lungu <james.lungu@stonybrook.edu>
2020-06-07 10:58:04 -0400

    Integrated search with navbar.

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -2,26 +2,35 @@ import React, { Component } from "react";
 import SearchBarComponent from "./SearchBarComponent";
 
 export class HomeComponent extends Component {
-    //This function will actually navigate to that page.
-    search = (e) =>{
-        const path = "/stock/" + document.getElementById('main-search').value
-        this.props.history.push(path)
-    }
+  //This function will actually navigate to that page.
+  search = (e) => {
+    e.preventDefault();
+    const path = "/stock/" + document.getElementById("main-search").value;
+    this.props.history.push(path);
+  };
+
+  componendDidMount() {
+    //On 'enter' key clicked, and if the text box is in focus, enter should SUBMIT...
+  }
 
   render() {
     return (
       <div class="home-div">
         <div class="input-group input-group-lg main-search">
-          <input
-          id="main-search"
-            type="text"
-            class="form-control"
-            aria-label="Large"
-            aria-describedby="inputGroup-sizing-sm"
-          />
+          <form onSubmit={this.search}>
+            <input
+              id="main-search"
+              type="text"
+              class="form-control"
+              aria-label="Large"
+              aria-describedby="inputGroup-sizing-sm"
+            />
             <div class="input-group-append">
-                <button class="btn btn-outline-secondary" type="button" onClick={this.search}>Submit</button>
+              <button class="btn btn-outline-secondary" type="submit">
+                Submit
+              </button>
             </div>
+          </form>
         </div>
       </div>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
+import { withRouter} from 'react-router-dom';
 
 export class Navbar extends Component {
+  //This function will actually navigate to that page.
+  search = (e) => {
+
+    const path = "/stock/" + document.getElementById("nav-search").value;
+    this.props.history.push(path);
+  };
+
   render() {
     return (
       <div>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark my-nav" >
           <a class="navbar-brand" href="#">
-            Fin-App
+          <i class="fas fa-landmark"></i>
           </a>
           <button
             class="navbar-toggler"
@@ -23,50 +31,21 @@ export class Navbar extends Component {
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
               <li class="nav-item active">
-                <a class="nav-link" href="#">
+                <a class="nav-link" href="/home">
                   Home <span class="sr-only">(current)</span>
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Link
-                </a>
-              </li>
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdown"
-                  role="button"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a class="dropdown-item" href="#">
-                    Action
-                  </a>
-                  <a class="dropdown-item" href="#">
-                    Another action
-                  </a>
-                  <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </div>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" href="#">
-                  Disabled
+              <li class="nav-item active">
+                <a class="nav-link" href="https://github.com/jlungu/fin-app">
+                  Source Code <span class="sr-only">(current)</span>
                 </a>
               </li>
             </ul>
-            <form class="form-inline my-2 my-lg-0">
+            <form class="form-inline my-2 my-lg-0" onSubmit={this.search.bind(this)}>
               <input
                 class="form-control mr-sm-2"
                 type="search"
+                id="nav-search"
                 placeholder="Search"
                 aria-label="Search"
               />
@@ -83,4 +62,4 @@ export class Navbar extends Component {
     );
   }
 }
-export default Navbar;
+export default withRouter(Navbar);
